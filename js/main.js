9f16a972cc11e54cb2348d3f201d624bf377a8f9
Sebastian <sebastian.r.rb@gmail.com>
2017-05-13 02:39:41 -0400

    Add selected button deactivation functionality

--- js/main.js
+++ js/main.js
@@ -32,49 +32,117 @@
             }
         }
 
-        //set classes
-        $clickedTile.siblings('.project-list__project').removeClass('selected');
-        $clickedTile.addClass('selected');
-        $(projectTiles).find('.selected-project-scanner').removeClass("selected-project-scanner--shown");
-        $clickedTile.find('.selected-project-scanner').addClass('selected-project-scanner--shown');
+        //if tile is already selected, remove selected class and reset description block
+        if($clickedTile.hasClass('selected')) {
+            projectID = 0;
+            for(var i = 0; i < projectObject.length; i++) {
+                if(projectID === projectObject[i].projectID) {
+                    selectedProjectObject = projectObject[i];
+                }
+            }
+            //disable clicked tile for a couple seconds
+             togglePointerEvents($(projectTiles));
 
-        //add demo button to both front and back
-        if(!selected) {
-           var demoButton = $('<a>').addClass('project-description__demo-button').text("View Project").attr('target', '_blank');
-           $back.append(demoButton);
-            setTimeout(function() {
-                var demoButton = $('<a>').addClass('project-description__demo-button').text("View Project").attr('target', '_blank');
-                $front.append(demoButton);
-            }, 1000);
-        }
-        selected = true;
+            $clickedTile.removeClass('selected');
 
-        //flip and change description
-        if($front.hasClass('visible-side')) {
-            //change info on back side, change visible-side class to back side, flip to back
-            console.log(projectObject.title);
-            console.log(projectObject.description);
-            $back.find('.project-description__project-title').text(selectedProjectObject.title);
-            $back.find('.project-description__project-caption').text(selectedProjectObject.description);
-            $back.find('.project-description__demo-button').attr('href', selectedProjectObject.demoLink);
+            if($back.hasClass('visible-side')) {
+                $front.find('.project-description__demo-button').remove();
+                setTimeout(function() {
+                    $back.find('.project-description__demo-button').remove();
+                }, 1000);
+            } else {
+                $back.find('.project-description__demo-button').remove();
+                setTimeout(function() {
+                    $front.find('.project-description__demo-button').remove();
+                }, 1000);
+            }
+            selected = false;
 
-            $front.removeClass('visible-side');
-            $back.addClass('visible-side');
+            $('.side:not(.visible-side)').find('.project-description__project-title').text(selectedProjectObject.title);
+            $('.side:not(.visible-side)').find('.project-description__project-caption').html(selectedProjectObject.description);
+             $('.side:not(.visible-side)').find('.project-description__demo-button').attr('href', selectedProjectObject.demoLink);
 
-            $flipper.addClass('flipped');
+             // $('.side.visible-side').removeClass('visible-side');
+             // $('.side:not(.visible-side)').addClass('visible-side');
+
+             // if($('.side:not(.visible-side)').find(".project-description__demo-button").length === 0) {
+             //     var demoButton = $('<a>').addClass('project-description__demo-button').text("View Project").attr('target', '_blank');
+
+             //     $('.side:not(.visible-side)').append(demoButton);
+             // }
+
+             if($front.hasClass('visible-side')) {
+                $front.removeClass('visible-side');
+                $back.addClass('visible-side');
+             } else {
+                $back.removeClass('visible-side');
+                $front.addClass('visible-side');
+             }
+
+             if($('.side:not(.visible-side)').find(".project-description__demo-button").length === 0) {
+                 var demoButton = $('<a>').addClass('project-description__demo-button').text("View Project").attr('target', '_blank');
+
+                 $('.side:not(.visible-side)').append(demoButton);
+             }
+
+             if($flipper.hasClass("flipped")) {
+                 $flipper.removeClass('flipped');
+             } else {
+                 $flipper.addClass('flipped');
+             }
+
+             // $flipper.toggleClass('flipped');
         } else {
-            //change info on front side, change visible-side class to front side, flip to front
-            $front.find('.project-description__project-title').text(selectedProjectObject.title);
-            $front.find('.project-description__project-caption').text(selectedProjectObject.description);
-            $front.find('.project-description__demo-button').attr('href', selectedProjectObject.demoLink);
+            //disable clicked tile for a couple seconds
+            togglePointerEvents($(projectTiles));
 
-            $back.removeClass('visible-side');
-            $front.addClass('visible-side');
+            //set classes
+            $clickedTile.siblings('.project-list__project').removeClass('selected');
+            $clickedTile.addClass('selected');
+            $(projectTiles).find('.selected-project-scanner').removeClass("selected-project-scanner--shown");
+            $clickedTile.find('.selected-project-scanner').addClass('selected-project-scanner--shown');
 
-            $flipper.removeClass('flipped');
+
+            if($('.side:not(.visible-side)').find(".project-description__demo-button").length === 0) {
+                console.log("Other side has no button...");
+                var demoButton = $('<a>').addClass('project-description__demo-button').text("View Project").attr('target', '_blank');
+
+                $('.side:not(.visible-side)').append(demoButton);
+            }
+            selected = true;
+
+            if($front.hasClass('visible-side')) {
+                //change info on back side, change visible-side class to back side, flip to back
+                $back.find('.project-description__project-title').text(selectedProjectObject.title);
+                $back.find('.project-description__project-caption').text(selectedProjectObject.description);
+                $back.find('.project-description__demo-button').attr('href', selectedProjectObject.demoLink);
+
+                $front.removeClass('visible-side');
+                $back.addClass('visible-side');
+
+                $flipper.addClass('flipped');
+            } else {
+                //change info on front side, change visible-side class to front side, flip to front
+                $front.find('.project-description__project-title').text(selectedProjectObject.title);
+                $front.find('.project-description__project-caption').text(selectedProjectObject.description);
+                $front.find('.project-description__demo-button').attr('href', selectedProjectObject.demoLink);
+
+                $back.removeClass('visible-side');
+                $front.addClass('visible-side');
+
+                $flipper.removeClass('flipped');
+            }
         }
 
     });
+
+
+    function togglePointerEvents(tiles) {
+        tiles.addClass('no-click');
+        setTimeout(function() {
+            tiles.removeClass('no-click');
+        }, 1800);
+    }
 
 
 
@@ -88,13 +156,13 @@
         title: "Evolution UI Framework",
         description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
         demoLink: "https://evolution-ui.github.io/evolution-ui/",
-        projectID: 2
+        projectID: 3
     },
     {
         title: "Developer Profile",
         description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
         demoLink: "http://moderndeveloper-students.github.io/coursework-sebastianrb/Course-07-Becoming-a-JavaScript-Expert/updated-bov-academy-portfolio/",
-        projectID: 3
+        projectID: 2
     },
     {
         title: "Tic Tac Toe",
@@ -107,7 +175,13 @@
         description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
         demoLink: "http://moderndeveloper-students.github.io/coursework-sebastianrb/Course-07-Becoming-a-JavaScript-Expert/Chapter-04-JavaScript-Events-In-Depth/Project-2-Jigsaw-Puzzle/",
         projectID: 5
-    }];
+    },
+    {
+        title: "Welcome!",
+        description: 'This is my front-end development portfolio. Here, you can check out a selection of projects I\'ve worked on; click a project from the list to see a description and a link to a demo. <br/><br/> To learn more about me, feel free to check out my "Developer Profile".',
+        projectID: 0
+    }
+    ];
 
 
 })();
