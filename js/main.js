5919b32550bd13a035b8932fcd778d3104bb0604
Sebastian <sebastian.r.rb@gmail.com>
2017-05-15 12:57:50 -0400

    Write project decriptions

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@
             }
             selected = false;
 
-            $('.side:not(.visible-side)').find('.project-description__project-title').text(selectedProjectObject.title);
+            $('.side:not(.visible-side)').find('.project-description__project-title').html(selectedProjectObject.title);
             $('.side:not(.visible-side)').find('.project-description__project-caption').html(selectedProjectObject.description);
              $('.side:not(.visible-side)').find('.project-description__demo-button').attr('href', selectedProjectObject.demoLink);
 
@@ -113,8 +113,8 @@
 
             if($front.hasClass('visible-side')) {
                 //change info on back side, change visible-side class to back side, flip to back
-                $back.find('.project-description__project-title').text(selectedProjectObject.title);
-                $back.find('.project-description__project-caption').text(selectedProjectObject.description);
+                $back.find('.project-description__project-title').html(selectedProjectObject.title);
+                $back.find('.project-description__project-caption').html(selectedProjectObject.description);
                 $back.find('.project-description__demo-button').attr('href', selectedProjectObject.demoLink);
 
                 $front.removeClass('visible-side');
@@ -123,8 +123,8 @@
                 $flipper.addClass('flipped');
             } else {
                 //change info on front side, change visible-side class to front side, flip to front
-                $front.find('.project-description__project-title').text(selectedProjectObject.title);
-                $front.find('.project-description__project-caption').text(selectedProjectObject.description);
+                $front.find('.project-description__project-title').html(selectedProjectObject.title);
+                $front.find('.project-description__project-caption').html(selectedProjectObject.description);
                 $front.find('.project-description__demo-button').attr('href', selectedProjectObject.demoLink);
 
                 $back.removeClass('visible-side');
@@ -148,42 +148,42 @@
 
     var projectObject = [{
         title: "Bov Academy Student Site",
-        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        description: "I am a lead developer on the team that built the student profile site for <a class='bov-link' href='https://bovacademy.com/' target='_blank'>Bov Academy</a>. The site contains profiles for the school's student body and heavily utilizes multimedia and various animation effects. We built the site using vanilla JavaScript.",
         demoLink: "https://students.bovacademy.com/",
         projectID: 1
     },
     {
         title: "Evolution UI Framework",
-        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        description: "Evolution UI is a working front-end framework comprising a library of innovative, unique web components, as well as a collection of standard components. I have contributed to and helped develop the project, and I now serve as a core maintainer.",
         demoLink: "https://evolution-ui.github.io/evolution-ui/",
         projectID: 3
     },
     {
         title: "Developer Profile",
-        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        description: "My developer profile is meant to help visitors get to know me better as both a developer and a person. The site contains a short bio as well as pictures and other information about my life and interests. It also includes contact information.",
         demoLink: "http://moderndeveloper-students.github.io/coursework-sebastianrb/Course-07-Becoming-a-JavaScript-Expert/updated-bov-academy-portfolio/",
         projectID: 2
     },
     {
         title: "Tic Tac Toe",
-        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        description: "This is a fully functional game of Tic Tac Toe played against the computer. The computer is moderately intelligent and will often try to block the player when the player is nearing victory. In addition, the computer will sometimes move first to keep things fair and interesting.",
         demoLink: "http://moderndeveloper-students.github.io/coursework-sebastianrb/Course-08-Art-of-Modern-Frontend-Development/Chapter-02-Mastering-jQuery/Project-2-tic-tac-toe/",
         projectID: 4
     },
     {
         title: "Jigsaw Puzzle",
-        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        description: "This is a working jigsaw puzzle. The user can ask for hints, reset the game as needed, and increase the difficulty. Be warned: the puzzle is fairly challenging, so pay close attention to the three hints!",
         demoLink: "http://moderndeveloper-students.github.io/coursework-sebastianrb/Course-07-Becoming-a-JavaScript-Expert/Chapter-04-JavaScript-Events-In-Depth/Project-2-Jigsaw-Puzzle/",
         projectID: 5
     },
     {
         title: "Welcome!",
-        description: 'This is my front-end development portfolio. Here, you can check out a selection of projects I\'ve worked on; click a project from the list to see a description and a link to a demo. <br/><br/> To learn more about me, feel free to check out my "Developer Profile".',
+        description: 'This is my front-end development portfolio. Here, you can check out a selection of projects I\'ve worked on; click a project from the list to see a description of the project and a link to it. <br/><br/> To learn more about me, feel free to check out my "Developer Profile".',
         projectID: 0
     },
     {
-        title: "JavaScript Utility Library",
-        description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        title: "JavaScript Utility Library <span class='wip'> (work in progress)</span>",
+        description: "The JavaScript utility library contains a number of useful functions and methods. The library is meant to help with common front-end tasks such as validating email addresses, shuffling arrays, filtering properties out of objects, and more.",
         demoLink: "https://github.com/sebastianrb/portfolio/tree/master/project-files/utilities.js",
         projectID: 6
     }
